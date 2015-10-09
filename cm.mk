78b5845200e29488550b01479f71dff7f9666cc8
vm03 <vm03@yandex.ru>
2015-10-10 00:31:19 +0300

    move to g3s-common

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_HEIGHT := 1280
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/jagnm/device.mk)
+$(call inherit-product, device/lge/g3s-common/device.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := jagnm
@@ -20,3 +20,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jagnm_global_com \
     BUILD_FINGERPRINT=lge/jagnm_global_com/jagnm:5.1.1/LMY48M/2167285:user/release-keys \
     PRIVATE_BUILD_DESC="jagnm_global_com-user 5.0.1 5.1.1 LMY48M 2167285 release-keys"
+
+$(call inherit-product, vendor/lge/jagnm/jagnm-vendor.mk)
